8ef0eb4ebb5faae8ba0c403982ce9681bdaba14e
Nikhil Singh <nikhilsingh93p@gmail.com>
2025-11-22 19:55:52 +0530

    java8

---
+++ src/main/java/com/springFrame/Filter_Even_Number_using_Stream.java
@@ -0,0 +1,21 @@
+package com.springFrame;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filter_Even_Number_using_Stream {
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9);
+
+        List<Integer> evenList = list.stream()
+                .filter( n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        // evenList.forEach(System.out::println);
+
+        evenList.forEach(item -> System.out.println("value : " + item));
+
+
+    }
+}
